a9debab17a58d0e1c395219217f6660e87fdfedc
Tobias Koppers <tobias.koppers@googlemail.com>
2017-03-07 20:05:08 +0100

    add support for mapping 1 to n lines

---
+++ test/mapGeneratedCode.js
@@ -0,0 +1,63 @@
+var should = require("should");
+var fs = require("fs");
+var path = require("path");
+var SourceListMap = require("../").SourceListMap;
+
+describe("mapGeneratedCode", function() {
+	it("should map generated code correctly", function() {
+		var map = new SourceListMap();
+		var source = [
+			"Normal Line 1",
+			"Normal Line 2",
+			"$",
+			"Normal Line 3",
+			"Line A;Line B;Line C",
+			"Line A;Line B;Line C",
+			"No\\",
+			"New\\",
+			"Line 1",
+			"No\\",
+			"$",
+			"New\\",
+			"$",
+			"$",
+			"Line 2",
+			"End Line"
+		].join("\n");
+		map.add(source + "\n", "file.txt", source + "\n");
+		map.add(source + "\n", "file.txt", source + "\n");
+		map.add(source + "\n");
+		map.add(source, "file.txt", source);
+		function mappingFunction(line) {
+			return line.replace(/;/g, "\n").replace(/\\\n/g, " ").replace(/\$\n/g, "");
+		}
+		var newMap = map.mapGeneratedCode(mappingFunction);
+		var result = newMap.toStringWithSourceMap({ file: "test.txt" });
+		var expectedPart = [
+			"AACA",
+			"AAEA",
+			"AACA",
+			"AAAA",
+			"AAAA",
+			"AACA",
+			"AAAA",
+			"AAAA",
+			"AACA,GACA,IACA",
+			"AACA,GAEA,IAGA",
+			"AACA"
+		].join(";");
+		result.map.mappings.should.be.eql([
+			"AAAA",
+			expectedPart,
+			"AAfA",
+			expectedPart,
+			"AACA",
+			expectedPart,
+			"AA/BA",
+			expectedPart
+		].join(";"));
+		result.source.should.be.eql(
+			mappingFunction([source, source, source, source].join("\n"))
+		);
+	});
+});
