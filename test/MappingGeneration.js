b766442485ca98adc23ab7fbfd68d7a9407c6609
Tobias Koppers <tobias.koppers@googlemail.com>
2017-05-04 11:39:37 +0200

    fixes webpack/webpack#4785

--- test/MappingGeneration.js
+++ test/MappingGeneration.js
@@ -1,5 +1,7 @@
 var should = require("should");
 var SourceListMap = require("../").SourceListMap;
+var SingleLineNode = require("../lib/SingleLineNode");
+var SourceNode = require("../lib/SourceNode");
 
 describe("MappingGeneration", function() {
 	it("should generate mappings", function() {
@@ -13,5 +15,18 @@ describe("MappingGeneration", function() {
 		result.source.should.be.eql("Gen\nCode Source\nCode\nGen Code Source\nCode");
 		result.map.sourcesContent[0].should.be.eql("Source\nCode\n");
 		result.map.mappings.should.be.eql(";A,KAAA;AACA;A,SADA;AACA");
+	})
+	it("should generate the same mappings for SingleLine and normal node", function() {
+		var map1 = new SourceListMap();
+		var map2 = new SourceListMap();
+		map1.add(new SingleLineNode("abc", "abc", "source", 10));
+		map2.add(new SourceNode("abc", "abc", "source", 10));
+		[map1, map2].forEach(map => {
+			map.add("\n\n");
+			map.add("Source Code\n", "file.txt", "Source\nCode\n");
+		})
+		var result1 = map1.toStringWithSourceMap({ file: "test.txt" });
+		var result2 = map2.toStringWithSourceMap({ file: "test.txt" });
+		result2.should.be.eql(result1);
 	})
 });
